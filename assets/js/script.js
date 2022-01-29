e63089b759c4d96a3358dc201fea95e3ffffdad9
RaoulRV <raoulradulescu@gmail.com>
2022-01-29 23:07:18 +0000

    Added js code for moving dog left and right and added the car in css

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("keydown", event => {
+    if(event.key==="ArrowLeft"){moveLeft();}
+    if(event.key==="ArrowRight"){moveRight();}
+  });
+var pablo=document.getElementById("pablo");
+function moveLeft(){
+        let left = parseInt(window.getComputedStyle(pablo).getPropertyValue("left"));
+        left -= 100;
+        if(left>=0){
+            pablo.style.left = left + "px";
+        }
+    }
+function moveRight(){
+            let left = parseInt(window.getComputedStyle(pablo).getPropertyValue("left"));
+            left += 100;
+            if(left<300){
+                pablo.style.left = left + "px";
+            }
+        }
+
+      
